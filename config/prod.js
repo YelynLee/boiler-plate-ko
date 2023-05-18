9a97b797a4777967847a0e6fb1cc88b419eda5a5
Yelyn Lee <yelynlee1004@gmail.com>
2023-05-18 10:25:22 +0900

    비밀정보 보호

---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+    mongoURl: process.env.MONGO_URI
+    //heroku에서 입력한 config vars key와 동일하게 'MONGO_URI' 작성한 것임
+}
--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 const express = require('express') /*express 모듈을 가져오기*/
 const app = express() /*express function을 통해 새로운 express app 생성*/
 const port = 5000
+const bodyParser = require('body-parser'); //bodyParser 가져오기
+const { User } = require("./models/User"); //User model 가져오기
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded 가져오기
+app.use(bodyParser.urlencoded({extended: true}));
+
+//application/json 가져오기
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://yelynlee1004:asdfqwer1234@boilerplate.ve9w8xq.mongodb.net/?retryWrites=true&w=majority', {
-    useNewUrlParser: true, useUnifiedTopology: true
+mongoose.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongDB Connected...'))
   .catch(err => console.log(err))
 
 
 app.get('/', (req, res) => res.send('Hello World!'))
 /*루트 directory에 오면 app이 'Hello World!' 출력*/
+
+app.post('/register', (req, res) => {
+  //client(postman)에 입력된 회원 정보를 body-parser가 server에서 가져오면 req.body로 저장 
+  //이를 database에 넣어주기
+
+  const user = new User(req.body) //instance 생성
+
+  user.save((err, userInfo) => {
+    if(err) return res.json({ success: false, err})
+    return res.status(200).json({ success: true })
+  })
+
+  //User model에 회원 정보를 저장
+  //몽고DB method(save)에 callback function; 저장 시 에러가 있으면 err 메시지
+                                                  //성공하면 status(200)
+
+})
+
+
+
+
+
 
 app.listen(port, () => console.log('Example app listening on port ${port}!'))
 /*port 5000에서 app의 실행*/
